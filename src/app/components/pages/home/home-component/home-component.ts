2717f388bba80b58c01178177d725acdbdd45dfb
Your Name <you@example.com>
2026-01-11 17:36:02 -0300

    adicionado componentes ao home

--- src/app/components/pages/home/home-component/home-component.ts
+++ src/app/components/pages/home/home-component/home-component.ts
@@ -1,20 +1,15 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from '../banner-component/banner-component';
+import { InfoBarComponent } from "../info-bar-component/info-bar-component";
+import { ProductCardComponent } from "../../../product/product-card-component/product-card-component";
 
 @Component({
   selector: 'app-home-component',
-  imports: [],
+  standalone: true,
+  imports: [BannerComponent, InfoBarComponent, ProductCardComponent], 
   templateUrl: './home-component.html',
-  styleUrl: './home-component.scss',
-  styles: [`
-    :host {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      min-height: 80vh;
-    }
-  `]
+  styleUrl: './home-component.scss'
 })
 export class HomeComponent {
 
 }
-
