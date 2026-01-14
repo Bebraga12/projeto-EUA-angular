fae0bf9dac64a2ab2e26b141f03b0f89e3d91bf5
Your Name <you@example.com>
2026-01-14 14:03:17 -0300

    menu components slide no work

--- src/app/components/layout/menu-component/menu-component.ts
+++ src/app/components/layout/menu-component/menu-component.ts
@@ -3,18 +3,18 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-menu-component',
-  standalone: true, // <--- 2. Certifique-se que está como true (se for projeto novo)
-  imports: [CommonModule], // <--- 3. Adicione o CommonModule aqui
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './menu-component.html',
   styleUrls: ['./menu-component.scss']
 })
 export class MenuComponent implements OnInit, OnDestroy {
-  
-  // Lista de imagens para o carrossel (adicione mais aqui se tiver)
+
+
   slides = [
-    { url: '/assets/banner/fundoHeader.png', alt: 'Midnight Artist Store' },
-    // Exemplo de como adicionar mais fotos:
-    // { url: 'assets/outra-foto.jpg', alt: 'Outra foto' } 
+    { url: 'assets/banner/fundoHeader.png', alt: 'Slide 1' },
+    { url: 'assets/banner/fundoHeader.png', alt: 'Slide 2' }, 
+    { url: 'assets/banner/fundoHeader.png', alt: 'Slide 3' }  
   ];
 
   currentSlideIndex = 0;
@@ -33,7 +33,7 @@ export class MenuComponent implements OnInit, OnDestroy {
   startSlider() {
     this.intervalId = setInterval(() => {
       this.nextSlide();
-    }, 5000); // Muda a foto a cada 5 segundos
+    }, 5000); 
   }
 
   nextSlide() {
